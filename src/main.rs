0a40f0670cab3762b77c5c3a9d1a3cbe0d9cd85a
Nicola Canzonieri <nicolacanzonieri2003@gmail.com>
2023-11-27 12:37:16 +0100

    Added clock's logic and settings page prototyped.

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,20 @@
+use slint::{Timer, TimerMode};
 slint::include_modules!();
 
 fn main() {
-    let app = Application::new().unwrap();
 
-    app.on_buttonPressed(move |value| {
-        match value.as_str() {
-            "play" => {
-                println!("Play!");
-            },
+    let application = Flow::new().unwrap();
 
-            "stop" => {
-                println!("Stop!");
-            },
+	let timer = Timer::default();
+	{
+		let application_weak = application.as_weak();
+		
+		timer.start(TimerMode::Repeated, std::time::Duration::from_millis(1000), move || {
+			let application = application_weak.unwrap();
+			application.invoke_tick(1000);
+		});
+	}
 
-            "pause" => {
-                println!("Pause!");
-            },
+    application.run().unwrap();
 
-            "settings" => {
-                println!("Settings");
-            },
-
-            _ => {
-                // nothing
-            }
-        }
-    });
-
-    app.run().unwrap();
 }
